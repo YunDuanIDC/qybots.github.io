f0368898a4b20e25de3322fc351ccb5b34ed1207
qybots <60844910+qybots@users.noreply.github.com>
2020-02-13 16:05:31 +0800

    Add files via upload

---
+++ js/qybot.main.js
@@ -0,0 +1,315 @@
+function Group1() {
+	var url = "https://jq.qq.com/?_wv=1027&k=5bxBeVG";
+	var iframe = document.createElement("iframe");
+	document.body.appendChild(iframe);
+	iframe.style.display = "none";
+	iframe.src = url;
+}
+
+function Group2() {
+	var url = "https://jq.qq.com/?_wv=1027&k=5Qe8J6c";
+	var iframe = document.createElement("iframe");
+	document.body.appendChild(iframe);
+	iframe.style.display = "none";
+	iframe.src = url;
+}
+
+function Statistics() {
+	window.open("https://www.cnzz.com/stat/website.php?web_id=1278608912")
+}
+
+function Home() {}
+
+function Community() {
+	xtip.closeAll();
+	xtip.msg('暂无社区', {
+		times: 4,
+		icon: 'w'
+	});
+}
+
+function Document() {
+	xtip.closeAll();
+	xtip.msg('暂无文档', {
+		times: 4,
+		icon: 'w'
+	});
+}
+
+function QYBot() {
+	var wx = window.navigator.userAgent.toLowerCase();
+	var qq = navigator.userAgent.toLowerCase();
+	if(wx.match(/MicroMessenger/i) == 'micromessenger') {
+		xtip.closeAll();
+		xtip.msg('微信不是浏览器,请使用浏览器下载!', {
+			times: 4,
+			icon: 'w'
+		});
+	} else {
+		if(qq.match(/QQ\/[0-9]/i)) {
+			xtip.closeAll();
+			xtip.msg('聊天工具不是浏览器,请使用浏览器下载!', {
+				times: 4,
+				icon: 'w'
+			});
+		} else {
+			var url = "http://qiniu.supner.com/o_1e0ro1p1816471ivu14j61f2hfhla.zip?attname=QY_Bot.zip";
+			var bturl = "";
+			xtip.closeAll();
+			xtip.msg('即将下载Android协议框架...', {
+				times: 8
+			});
+			window.open(url)
+
+			/*var iframe = document.createElement("iframe");
+			document.body.appendChild(iframe);
+			iframe.style.display = "none";
+			iframe.src = url;*/
+
+			/*document.getElementById('Donation').setAttribute('href', url);
+			var link = document.getElementById('Donation');
+			link.click();*/
+		}
+	}
+}
+
+function QYBotch() {
+	var wx = window.navigator.userAgent.toLowerCase();
+	var qq = navigator.userAgent.toLowerCase();
+	if(wx.match(/MicroMessenger/i) == 'micromessenger') {
+		xtip.closeAll();
+		xtip.msg('微信不是浏览器,请使用浏览器下载!', {
+			times: 4,
+			icon: 'w'
+		});
+	} else {
+		if(qq.match(/QQ\/[0-9]/i)) {
+			xtip.closeAll();
+			xtip.msg('聊天工具不是浏览器,请使用浏览器下载!', {
+				times: 4,
+				icon: 'w'
+			});
+		} else {
+			var url = "http://qiniu.supner.com/o_1e0ro4l5l1qg410da1qs614u22o5a.zip?attname=QY_Botch.zip";
+			var bturl = "";
+
+			xtip.closeAll();
+			xtip.msg('即将下载Android协议批量框架...', {
+				times: 8
+			});
+
+			window.open(url)
+
+			/*var iframe = document.createElement("iframe");
+			document.body.appendChild(iframe);
+			iframe.style.display = "none";
+			iframe.src = url;*/
+
+			/*document.getElementById('Donation').setAttribute('href', url);
+			var link = document.getElementById('Donation');
+			link.click();*/
+		}
+	}
+}
+
+function QYPCBot() {
+	var wx = window.navigator.userAgent.toLowerCase();
+	var qq = navigator.userAgent.toLowerCase();
+	if(wx.match(/MicroMessenger/i) == 'micromessenger') {
+		xtip.closeAll();
+		xtip.msg('微信不是浏览器,请使用浏览器下载!', {
+			times: 4,
+			icon: 'w'
+		});
+	} else {
+		if(qq.match(/QQ\/[0-9]/i)) {
+			xtip.closeAll();
+			xtip.msg('聊天工具不是浏览器,请使用浏览器下载!', {
+				times: 4,
+				icon: 'w'
+			});
+		} else {
+			var url = "http://qiniu.supner.com/o_1e0ro6esn1ohh1bqupbi1rvd155va.zip?attname=QY_Botch.zip";
+			var bturl = "";
+
+			xtip.closeAll();
+			xtip.msg('即将下载PC协议框架...', {
+				times: 8
+			});
+
+			window.open(url)
+
+			/*var iframe = document.createElement("iframe");
+			document.body.appendChild(iframe);
+			iframe.style.display = "none";
+			iframe.src = url;*/
+
+			/*document.getElementById('Donation').setAttribute('href', url);
+			var link = document.getElementById('Donation');
+			link.click();*/
+		}
+	}
+}
+
+function E_SDK() {
+	var wx = window.navigator.userAgent.toLowerCase();
+	var qq = navigator.userAgent.toLowerCase();
+	if(wx.match(/MicroMessenger/i) == 'micromessenger') {
+		xtip.closeAll();
+		xtip.msg('微信不是浏览器,请使用浏览器下载!', {
+			times: 4,
+			icon: 'w'
+		});
+	} else {
+		if(qq.match(/QQ\/[0-9]/i)) {
+			xtip.closeAll();
+			xtip.msg('聊天工具不是浏览器,请使用浏览器下载!', {
+				times: 4,
+				icon: 'w'
+			});
+		} else {
+			var url = "http://qiniu.supner.com/o_1e0ukvbp06uu1vm31f8p19duaoha.zip?attname=E_SDK.zip";
+			var bturl = "https://link.gimhoy.com/sharepoint/aHR0cHM6Ly9qaWE2NjYtbXkuc2hhcmVwb2ludC5jb20vOnU6L2cvcGVyc29uYWwvcWlhbmNoYW5ndHVqdW5feGt4X21lL0VjTEktdGQyUnIxQmtiOGJoUUVmM3ZNQkZPd0h1Vy1GSURJb2dqbnhJcDg1LWc/ZT1yN3Q0Q3g=.zip";
+
+			xtip.closeAll();
+			xtip.msg('即将下载易语言插件模板...', {
+				times: 8
+			});
+
+			window.open(url)
+
+			/*var iframe = document.createElement("iframe");
+			document.body.appendChild(iframe);
+			iframe.style.display = "none";
+			iframe.src = url;*/
+
+			/*document.getElementById('Donation').setAttribute('href', url);
+			var link = document.getElementById('Donation');
+			link.click();*/
+		}
+	}
+}
+
+function C_SDK() {
+	var wx = window.navigator.userAgent.toLowerCase();
+	var qq = navigator.userAgent.toLowerCase();
+	if(wx.match(/MicroMessenger/i) == 'micromessenger') {
+		xtip.closeAll();
+		xtip.msg('微信不是浏览器,请使用浏览器下载!', {
+			times: 4,
+			icon: 'w'
+		});
+	} else {
+		if(qq.match(/QQ\/[0-9]/i)) {
+			xtip.closeAll();
+			xtip.msg('聊天工具不是浏览器,请使用浏览器下载!', {
+				times: 4,
+				icon: 'w'
+			});
+		} else {
+			var url = "http://qiniu.supner.com/o_1e0ul1789s074jvruu1q3dihua.zip?attname=C_SDK.zip";
+			var bturl = "https://link.gimhoy.com/sharepoint/aHR0cHM6Ly9qaWE2NjYtbXkuc2hhcmVwb2ludC5jb20vOnU6L2cvcGVyc29uYWwvcWlhbmNoYW5ndHVqdW5feGt4X21lL0VUMWhfZDJMVmc1QW9pY3g0Q01fZXRjQmY3U1MxUlB2RjVESUgzZzdXX3RycHc/ZT11WmUzNFo=.zip";
+
+			xtip.closeAll();
+			xtip.msg('即将下载C#插件模板...', {
+				times: 8
+			});
+
+			window.open(url)
+
+			/*var iframe = document.createElement("iframe");
+			document.body.appendChild(iframe);
+			iframe.style.display = "none";
+			iframe.src = url;*/
+
+			/*document.getElementById('Donation').setAttribute('href', url);
+			var link = document.getElementById('Donation');
+			link.click();*/
+
+		}
+	}
+}
+
+function C__SDK() {
+	var wx = window.navigator.userAgent.toLowerCase();
+	var qq = navigator.userAgent.toLowerCase();
+	if(wx.match(/MicroMessenger/i) == 'micromessenger') {
+		xtip.closeAll();
+		xtip.msg('微信不是浏览器,请使用浏览器下载!', {
+			times: 4,
+			icon: 'w'
+		});
+	} else {
+		if(qq.match(/QQ\/[0-9]/i)) {
+			xtip.closeAll();
+			xtip.msg('聊天工具不是浏览器,请使用浏览器下载!', {
+				times: 4,
+				icon: 'w'
+			});
+		} else {
+			var url = "http://qiniu.supner.com/o_1e0ul1ndkdd01lloa1fss1jaoa.zip?attname=C__SDK.zip";
+			var bturl = "";
+
+			xtip.closeAll();
+			xtip.msg('即将下载C++插件模板...', {
+				times: 8
+			});
+
+			window.open(url)
+
+			/*$.ajax({
+				url: url,
+				type: 'GET',
+				complete: function(response) {
+					if(response.status == 200) {
+						window.open(url)
+					} else {
+						window.open(bturl)
+					}
+				}
+			});*/
+
+			/*var iframe = document.createElement("iframe");
+			document.body.appendChild(iframe);
+			iframe.style.display = "none";
+			iframe.src = url;*/
+
+			/*document.getElementById('Donation').setAttribute('href', url);
+			var link = document.getElementById('Donation');
+			link.click();*/
+
+		}
+	}
+}
+
+function Donation() {
+	var url = "./donation.html";
+	document.getElementById('Donation').setAttribute('href', url);
+	var link = document.getElementById('Donation');
+	link.click();
+	/*xtip.closeAll();
+	xtip.msg('暂时无需捐助', {
+		times: 4,
+		icon: 'w'
+	});*/
+}
+
+function Upyun() {
+	window.open("https://www.upyun.com/")
+
+}
+
+function Github() {
+	window.open("https://github.com/")
+}
+
+function Coding() {
+	window.open("https://coding.net/")
+}
+
+window.onload = function() {　　　　
+	/*xtip.closeAll();
+	xtip.alert('管理员正在修改代码...', 'w', {
+		title: '提示'
+	});*/
+}
